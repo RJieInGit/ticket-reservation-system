eb7ef0e8f0edce5b4bfa27e5bf51421167ef293f
任杰 <renjie@ninmasarus-MacBook-puro.local>
2019-09-28 20:09:13 -0400

    debug

--- code/threads/admin.cc
+++ code/threads/admin.cc
@@ -88,7 +88,7 @@ void admin:: createReservation(){
 
 }
 
-void admin:: requestThread(int requestID){
+void admin:: requestThread(int *requestID){
      int start = rand()%19;         //can not start from last station
      int destiny = rand()%(19-start);   
      destiny+=start;
@@ -144,7 +144,7 @@ void admin :: createTrain(){
         trainID++;
 }
 
-void admin::trainThread(int &trainID){
+void admin::trainThread(int *trainID){
     int currentstation=-1;
     // assuming 10min to arrive at next station.
   while(currentstation<=20){
